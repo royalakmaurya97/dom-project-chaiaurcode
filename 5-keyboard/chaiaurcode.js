596bdc73726b4f4b7b2ca32d04a239600a619f32
royalakmaurya97 <kumaralok911863@gmail.com>
2024-09-28 17:43:47 +0530

    Keboard key code

--- 5-keyboard/chaiaurcode.js
+++ 5-keyboard/chaiaurcode.js
@@ -1,1 +1,23 @@
-console.log('Project 5');
+// console.log('Project 5');
+
+
+const insert = document.getElementById('insert');
+
+window.addEventListener('keydown', (e) => {
+    insert.innerHTML = `
+    <div class='color'>
+    <table>
+  <tr>
+    <th>key</th>
+    <th>KeyCode</th>
+    <th>Code</th>
+  </tr>
+  <tr>
+    <td>${e.key === ' ' ? 'Space': e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+</table>
+    </div>
+    `
+});
